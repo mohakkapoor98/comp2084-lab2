1731315cffc1ca69c8d66f84f75b645f65fe7910
Mohakkapoor98 <200425207@student.georgianc.on.ca>
2020-04-11 21:25:52 -0400

    Completed Lab-2

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            string[] products = { "Laptop", "Tablet", "Desktop" };
+            ViewData["products"] = products;
+            return View();
+        }
+        public String Details(string product)
+        {
+            string prompt = "You have selected " + product;
+            return prompt;
+        }
+
+    }
+}
